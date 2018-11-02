200bf5429dc7793fe7fa14d399219fbad32bb919
jugglingcats <alfie@glowbuzzer.com>
2018-11-02 10:47:45 +0000

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace PortView
+{
+    partial class PortViewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PortViewForm));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.alwaysOnTopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.floatingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.floatingToolStripMenuItem,
+            this.alwaysOnTopToolStripMenuItem,
+            this.resetToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(152, 92);
+            // 
+            // alwaysOnTopToolStripMenuItem
+            // 
+            this.alwaysOnTopToolStripMenuItem.Checked = true;
+            this.alwaysOnTopToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.alwaysOnTopToolStripMenuItem.Enabled = false;
+            this.alwaysOnTopToolStripMenuItem.Name = "alwaysOnTopToolStripMenuItem";
+            this.alwaysOnTopToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.alwaysOnTopToolStripMenuItem.Text = "Always on &Top";
+            this.alwaysOnTopToolStripMenuItem.Click += new System.EventHandler(this.alwaysOnTopToolStripMenuItem_Click);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.resetToolStripMenuItem.Text = "&Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "925801-32 (2).png");
+            this.imageList1.Images.SetKeyName(1, "925801-32 (1).png");
+            // 
+            // floatingToolStripMenuItem
+            // 
+            this.floatingToolStripMenuItem.Checked = true;
+            this.floatingToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.floatingToolStripMenuItem.Name = "floatingToolStripMenuItem";
+            this.floatingToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.floatingToolStripMenuItem.Text = "&Floating";
+            this.floatingToolStripMenuItem.Click += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Enabled = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Margin = new System.Windows.Forms.Padding(12, 3, 3, 30);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(800, 450);
+            this.listView1.TabIndex = 1;
+            this.listView1.TileSize = new System.Drawing.Size(200, 50);
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Tile;
+            // 
+            // PortViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Name = "PortViewForm";
+            this.Text = "Port Viewer";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem alwaysOnTopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem floatingToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Management;
+using System.IO.Ports;
+using System.Threading;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace PortView
+{
+    public partial class PortViewForm : Form
+    {
+        private const int WM_DEVICECHANGE = 0x219;
+
+        private int GWL_EXSTYLE = -20;
+
+        private bool updating;
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x20 | 0x80000 | 0x80; // 0x80000 /* WS_EX_LAYERED */ /*| 0x20 WS_EX_TRANSPARENT */ /*| 0x80 WS_EX_TOOLWINDOW */;
+                return cp;
+            }
+        }
+
+        public PortViewForm()
+        {
+            InitializeComponent();
+
+            ShowInTaskbar = false;
+
+            listView1.SmallImageList = imageList1;
+            listView1.LargeImageList = imageList1;
+            //            listView1.StateImageList = imageList1;
+            listView1.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+
+            BackColor = Color.LimeGreen;
+            TransparencyKey = Color.LimeGreen;
+
+            StartPosition = FormStartPosition.CenterScreen;
+            Size = new System.Drawing.Size(250, 150);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;  // no borders
+
+            TopMost = true;
+            Visible = true;
+
+            UpdatePorts();
+        }
+
+        private string desc(uint pid, ManagementBaseObject p)
+        {
+            switch (pid)
+            {
+                case 0x5740:
+                    return "Espruino";
+                case 0x5742:
+                    return "Glowboard";
+                case 0x374b:
+                    return "Nucleo";
+                default:
+                    return p["Description"].ToString();
+            }
+        }
+
+        private ListEntry itemise(ManagementBaseObject p)
+        {
+            var result = new ListEntry
+            {
+                Port = p["DeviceID"].ToString(),
+                Caption = p.Properties["Caption"].ToString()
+            };
+
+            Regex r = new Regex("USB\\\\VID_([0-9A-F]+)&PID_([0-9A-F]+)");
+            Match m = r.Match(p["PNPDeviceId"].ToString());
+            if (m.Success)
+            {
+                var vid = Convert.ToUInt32(m.Groups[1].ToString(), 16);
+                var pid = Convert.ToUInt32(m.Groups[2].ToString(), 16);
+                result.VID = vid;
+                result.PID = pid;
+                if (vid == 0x0483)
+                {
+                    result.Caption = $"{desc(pid, p)} ({p["DeviceID"].ToString()})";
+                }
+            }
+            return result;
+        }
+
+        private IEnumerable<ListEntry> EnumPorts()
+        {
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM WIN32_SerialPort"))
+            {
+                string[] portnames = SerialPort.GetPortNames();
+
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList();
+                return (from n in portnames
+                        join p in ports on n equals p["DeviceID"].ToString()
+                        select itemise(p)).ToList();
+            }
+        }
+
+        private void Flicker(ListViewItem item, int image)
+        {
+            Task.Run(() =>
+            {
+                for (int n = 0; n < 10; n++)
+                {
+                    Thread.Sleep(200);
+                    this.BeginInvoke((MethodInvoker)delegate
+                    {
+                        item.ImageIndex = n % 2 == 0 ? image : 10;
+                    });
+                }
+            });
+        }
+
+        private void UpdatePorts(bool first = false)
+        {
+            if (this.updating)
+            {
+                return;
+            }
+            this.updating = true;
+            Task.Run(() =>
+            {
+                Thread.Sleep(200); // delay
+
+                this.BeginInvoke((MethodInvoker)delegate
+                {
+                    var items = EnumPorts();
+                    var existing = listView1.Items.Cast<ListViewItem>().Select(i => (ListEntry)i.Tag);
+
+                    foreach (var item in existing.Where(p => !p.Connected && items.Any(p2 => p2.Equals(p))))
+                    {
+                        item.Connected = true;
+                        var listViewItem = listView1.Items.Find(item.Port, false);
+                        Debug.WriteLine($"{1} was re-connected", item.Caption);
+
+                        if (listViewItem.Length > 0)
+                        {
+                            Flicker(listViewItem[0], 1);
+                        }
+                    }
+                    foreach (var item in items.Where(p => !existing.Any(p2 => p2.Equals(p))))
+                    {
+                        item.Connected = true;
+                        Debug.WriteLine($"{1} was connected", item.Caption);
+                        var listViewItem = listView1.Items.Add(item.Port, item.Caption, 1);
+                        listViewItem.Tag = item;
+                    }
+                    foreach (var item in existing.Where(p => !items.Any(p2 => p2.Equals(p))))
+                    {
+                        item.Connected = false;
+                        var listViewItem = listView1.Items.Find(item.Port, false);
+                        Debug.WriteLine($"{1} was disconnected", item.Caption);
+
+                        if ( listViewItem.Length > 0 )
+                        {
+                            Flicker(listViewItem[0], 0);
+                        }
+                    }
+                    this.updating = false;
+                });
+            });
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            switch (m.Msg)
+            {
+                case WM_DEVICECHANGE:
+                    Task.Delay(500).ContinueWith((task) => { UpdatePorts(); });
+                    break;
+            }
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            SetTransparency();
+        }
+
+        private void SetTransparency()
+        {
+            User32_SetLayeredWindowAttributes(this.Handle, (TransparencyKey.B << 16) + (TransparencyKey.G << 8) + TransparencyKey.R, 160, 0x01 | 0x02);
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            int style = GetWindowLong(this.Handle, GWL_EXSTYLE);
+            if ((style & 0x80000) == 0x80000)
+            {
+                FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+                SetWindowLong(this.Handle, GWL_EXSTYLE, style & ~(0x20 | 0x80000 | 0x80));
+                floatingToolStripMenuItem.Checked = false;
+                alwaysOnTopToolStripMenuItem.Enabled = true;
+                TopMost = alwaysOnTopToolStripMenuItem.Checked;
+            }
+            else
+            {
+                FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+                SetWindowLong(this.Handle, GWL_EXSTYLE, style | (0x20 | 0x80000 | 0x80));
+                SetTransparency();
+                floatingToolStripMenuItem.Checked = true;
+                alwaysOnTopToolStripMenuItem.Enabled = false;
+                TopMost = true;
+            }
+            Update();
+        }
+
+        private void alwaysOnTopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            alwaysOnTopToolStripMenuItem.Checked = !alwaysOnTopToolStripMenuItem.Checked;
+            TopMost = alwaysOnTopToolStripMenuItem.Checked;
+            Update();
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            UpdatePorts();
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetLayeredWindowAttributes")]
+        private static extern bool User32_SetLayeredWindowAttributes(IntPtr hWnd, int crKey, byte bAlpha, int attributes);
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+
+    class ListEntry
+    {
+        public uint VID;
+        public uint PID;
+        public string Port;
+        public string Caption;
+
+        public bool Connected { get; internal set; }
+
+        public override bool Equals(object obj)
+        {
+            ListEntry other = obj as ListEntry;
+
+            return other.VID == this.VID && other.PID == this.PID;
+        }
+
+        public override int GetHashCode()
+        {
+            return (int)(VID + PID);
+        }
+    }
+}
